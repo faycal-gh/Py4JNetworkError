97ec08855dff4088756c4a59e8d912dc739c8eeb
faycal-gh <57934889+faycal-gh@users.noreply.github.com>
2020-07-31 18:20:15 +0200

    Update py4j.py

--- py4j.py
+++ py4j.py
@@ -1,4 +1,4 @@
->>> import py4j
+>>> import py4j #pip install py4j
 >>> from py4j.java_gateway import JavaGateway
 >>> gateway = JavaGateway()                   # connect to the JVM
 >>> py4j.java_gateway.launch_gateway(port=0, jarpath='', classpath='', javaopts=[], die_on_exit=False, redirect_stdout=None, redirect_stderr=None, daemonize_redirect=True, java_path='java', create_new_process_group=False, enable_auth=False, cwd=None, return_proc=False)
